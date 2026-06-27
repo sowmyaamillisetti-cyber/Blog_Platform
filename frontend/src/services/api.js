6d0d4197522ff149a8f31e7a3dec6475a8ee927a
Sowmya <your-github-email@example.com>
2026-06-27 13:07:16 +0530

    Fixed API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-platform-fkxj.onrender.com",
+  baseURL: "https://blog-platform-fkxj.onrender.com/api",
 });
 
 export default API;
